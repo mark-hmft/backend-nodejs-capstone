64be93db7636d7dbf577b6ad0bcfa9c9eda31670
mark-hmft <166622903+mark-hmft@users.noreply.github.com>
2024-07-09 19:40:10 -0600

    Update authRoutes.js

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -19,18 +19,18 @@ router.post('/register', async (req, res) => {
     try {
       //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
       const db = await connectToDatabase();
+      //Access the `users` collection
       const collection = db.collection("users");
+      //Check for existing email in DB
       const existingEmail = await collection.findOne({ email: req.body.email });
-
         if (existingEmail) {
             logger.error('Email id already exists');
             return res.status(400).json({ error: 'Email id already exists' });
         }
-
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email=req.body.email;
-        console.log('email is',email);
+        //Save user details
         const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
@@ -38,13 +38,12 @@ router.post('/register', async (req, res) => {
             password: hash,
             createdAt: new Date(),
         });
-
         const payload = {
             user: {
                 id: newUser.insertedId,
             },
         };
-
+        //Create JWT
         const authtoken = jwt.sign(payload, JWT_SECRET);
         logger.info('User registered successfully');
         res.json({ authtoken,email });
@@ -53,34 +52,37 @@ router.post('/register', async (req, res) => {
         return res.status(500).send('Internal server error');
     }
 });
-
+    //Login Endpoint
 router.post('/login', async (req, res) => {
     console.log("\n\n Inside login")
-
     try {
-        // const collection = await connectToDatabase();
+        // connect to `secondChance` in MongoDB through `connectToDatabase`
         const db = await connectToDatabase();
+        //Access MongoDB `users` collection
         const collection = db.collection("users");
+        //Check for user credentials in database
         const theUser = await collection.findOne({ email: req.body.email });
-
+        //Check if the password matches
         if (theUser) {
             let result = await bcryptjs.compare(req.body.password, theUser.password)
+            //send appropriate message if mismatch
             if(!result) {
                 logger.error('Passwords do not match');
                 return res.status(404).json({ error: 'Wrong pasword' });
             }
+            //Fetch user details
             let payload = {
                 user: {
                     id: theUser._id.toString(),
                 },
             };
-
             const userName = theUser.firstName;
             const userEmail = theUser.email;
-
+            //Create JWT authentication if passwords match
             const authtoken = jwt.sign(payload, JWT_SECRET);
             logger.info('User logged in successfully');
             return res.status(200).json({ authtoken, userName, userEmail });
+        //Send appropriate message if user not found
         } else {
             logger.error('User not found');
             return res.status(404).json({ error: 'User not found' });
